611ba30fe595304f8305afed7679962a8a8bb3d7
Rifqi Fadhlurrakhman <43606903+rifqifdr@users.noreply.github.com>
2021-03-31 21:11:19 +0700

    first commit

---
+++ tc-config.js
@@ -0,0 +1,20 @@
+/* Tin Can configuration */
+
+//
+// ActivityID that is sent for the statement's object
+//
+TC_COURSE_ID = "http://GB9umkVX7_cCMyByzVHmBWOn9C6HXKRP_rise"
+
+//
+// CourseName for the activity
+//
+TC_COURSE_NAME = {
+  "en-US": "Demo Kelas Prakerja"
+};
+
+//
+// CourseDesc for the activity
+//
+TC_COURSE_DESC = {
+  "en-US": "&lt;p&gt;Salah satu hal yang kalian bisa lakukan untuk menggunakan media sosial untuk bisnis kalian adalah melakukan social media advertising. Social media advertising merupakan pengiklanan melalui media sosial. Walaupun terdengar sederhana, banyak sekali variabel yang terdapat pada social media advertising termasuk platform pilihan, audiens, demografi, konten, serta tools yang bisa digunakan, sehingga social media advertising pun memiliki ruang pembelajaran yang cukup luas dan mendalam.&lt;/p&gt;&lt;p&gt;&lt;br&gt;&lt;a href=&#34;http://%3Ciframe%20src=%22https%3A//lingkaran-prakerja.tribe.so/group/kelas-management-hukum%22%20name=%22iframe_a%22%20height=%22600px%22%20width=%22100%%22%20title=%22Iframe%20Example%22%3E%3C/iframe%3E&#34; rel=&#34;noopener noreferrer&#34; target=&#34;_blank&#34;&gt;&lt;strong&gt;&lt;span style=&#34;color: rgb(237, 106, 81);&#34;&gt;&lt;/span&gt;&lt;/strong&gt;&lt;/a&gt;&lt;/p&gt;"
+};
